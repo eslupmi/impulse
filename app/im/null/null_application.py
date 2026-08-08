f292ed79d25ed9c6040e7a3f2e813ae76352bd2e
tansdf <tansdf@mail.ru>
2026-08-08 22:32:08 +0300

    Add ui_user to NullApplication freeze action signature.

--- app/im/null/null_application.py
+++ app/im/null/null_application.py
@@ -109,7 +109,7 @@ class NullApplication(Application):
         return {}
 
     async def _handle_freeze_action(self, incident_: 'Incident', freeze_option: str, user_id: str, incidents,
-                                    queue_: 'AsyncQueue', user_timezone: Optional[str] = None):
+                                    queue_: 'AsyncQueue', user_timezone: Optional[str] = None, ui_user=None):
         return
 
     async def post_freeze_notification(self, incident_: 'Incident', ui_user=None):
